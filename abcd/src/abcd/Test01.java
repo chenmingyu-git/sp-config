8d9df7a76c5c034dd40ace7cf8bfedb7a36bbdef
TEDU <TEDU@DESKTOP-IKGNEJ6>
2019-12-02 21:09:42 +0800

    df

--- abcd/src/abcd/Test01.java
+++ abcd/src/abcd/Test01.java
@@ -4,5 +4,11 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("hahaha");
+		
+	}
+	
+	
+	public void test01() {
+		System.out.println("hahha");
 	}
 }
